e94b4b6a16ebf11f794904fd51052a2ea485b353
Jagadish Pamarthi <jagadishpamarthi@MacBook-Pro.fios-router.home>
2017-02-20 13:14:23 -0500

    if-then-else example

---
+++ IfThenElse.java
@@ -0,0 +1,41 @@
+/*when an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird*/
+
+ import java.io.*;
+    import java.util.*;
+    import java.text.*;
+    import java.math.*;
+    import java.util.regex.*;
+
+    public class IfThenElse {
+
+        public static void main(String[] args) {
+
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();            
+            String ans="";
+            if(n%2==1){
+              ans = "Weird";
+            }
+            else{
+            
+               //Complete the code
+                if(n >= 2 && n <= 5) {
+	               ans = "Not Weird";
+	           } else if(n >= 6 && n <= 20) {
+	               ans = "Weird";
+	           } else {
+	               ans = "Not Weird";
+	           }
+                
+            }
+            System.out.println(ans);
+            
+        }
+    }
+
+
